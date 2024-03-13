62d282924d50298dfd62d6ee1dd814cd929c9ce2
tarasend <tz.taras@gmail.com>
2024-03-13 14:21:08 +0200

    <h3>{item.title}</h3>                     <p>{item.body}</p>                     <a>{item.href}</a>

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,13 +1,22 @@
 import './Main.css';
+import articles from '../data/articles.json';
+console.log(articles);
 function Main() {
     return (
-    <div className="main">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem alias, eos magni delectus quibusdam iusto reprehenderit repudiandae pariatur nobis. Voluptatum suscipit cumque, molestiae nulla, ab natus deleniti quo beatae eos neque, voluptas velit modi aliquam. Et beatae ipsum quis nobis sunt. Nulla perferendis modi odio laudantium ipsam officiis assumenda enim consequuntur saepe praesentium. Voluptate nemo neque dolore repellendus quaerat culpa libero quas dolores ut, voluptatem consectetur, asperiores corporis recusandae, eveniet explicabo delectus minus amet expedita debitis sed unde. Ipsa ea iure possimus. Nam alias inventore quaerat consectetur quibusdam vitae sit animi magnam velit rem eligendi, quas modi quod. Numquam, delectus?
+        <div className="main">
+            {/* {articles.map(item => <h2>{item.title}</h2>)} */}
+            {articles.map(item =>
+                <section>
+                    <h3>{item.title}</h3>
+                    <p>{item.body}</p>
+                    <a>{item.href}</a>
 
-
-
-        </p>
-        </div>
+                </section>)}
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem alias, eos magni delectus quibusdam iusto reprehenderit repudiandae pariatur nobis. Voluptatum suscipit cumque, molestiae nulla, ab natus deleniti quo beatae eos neque, voluptas velit modi aliquam. Et beatae ipsum quis nobis sunt. Nulla perferendis modi odio laudantium ipsam officiis assumenda enim consequuntur saepe praesentium. Voluptate nemo neque dolore repellendus quaerat culpa libero quas dolores ut, voluptatem consectetur, asperiores corporis recusandae, eveniet explicabo delectus minus amet expedita debitis sed unde. Ipsa ea iure possimus. Nam alias inventore quaerat consectetur quibusdam vitae sit animi magnam velit rem eligendi, quas modi quod. Numquam, delectus?
+            </p>
+            
+            
+        </div> 
     )
 }
 
