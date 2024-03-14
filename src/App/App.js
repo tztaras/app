55a9f3639858ee7e904fdab82985edfc1a078b76
tarasend <tz.taras@gmail.com>
2024-03-14 20:46:44 +0200

    JSX

--- src/App/App.js
+++ src/App/App.js
@@ -8,6 +8,8 @@ function App() {
     return (
         <div className='container'>
             <Header/>
+            
+                
             <Main/>
             <Sidebar/>
             <Footer/>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,8 +2,39 @@ import './Main.css';
 import articles from '../data/articles.json';
 console.log(articles);
 function Main() {
+
+    let q = 3;
+    const m = [1, 2, 3, 4, 5];
+    let c = { 'background': 'cadetblue' };
+    let nc = 'aliceblue';
+
     return (
         <div className="main">
+
+            <section>
+                <h3>Вивід змінної </h3>
+                <div>{q + q*q}</div>
+                <div>{'{}'}</div>
+
+                <h3>Вивід массиву </h3>
+                <div>{m}</div>
+                <div>{m.join('^')}</div>
+
+                <h3>Методи массивів </h3>
+                <div>{m.map(item=>item)}</div>
+                <div>{m.map(item => item + '^')}</div>
+                
+                <h3>Методи массивів -  створення елементів </h3>
+                <div>{m.map(item => <b>{item + ' '}</b>)}</div>
+                
+                <h3>Стилі inline</h3>
+                <p style={c}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, rerum possimus, cupiditate nemo sunt et sed saepe aliquid quasi minima dolorem necessitatibus quos velit esse modi nobis vitae architecto maiores.</p>
+                <p style={{ color: nc, background:'cadetblue' }}  >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, rerum possimus, cupiditate nemo sunt et sed saepe aliquid quasi minima dolorem necessitatibus quos velit esse modi nobis vitae architecto maiores.</p>
+
+
+
+
+            </section>
             {/* {articles.map(item => <h2>{item.title}</h2>)} */}
             {articles.map(item =>
                 <section>
