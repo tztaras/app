9ea0bac4dce840b543faa2e9b2c042ad6dcd4aff
tarasend <tz.taras@gmail.com>
2024-03-21 16:33:08 +0200

    React.createRef()

--- src/About/About.js
+++ src/About/About.js
@@ -1,11 +1,16 @@
 import '../Main/Main.css';
 import './About.css';
+import React from 'react';
 function About() {
+    // let textInput = React.createRef();
+    // let textOutput = React.createRef();
+    // textOutput.current.innerHTML = textInput.current.value;
     return (
         <>
             <div className='about'>
                 <section>
-                    <h1>ABOUT</h1>
+                    <h1>About</h1>
+                    {/* <p ref={textOutput}></p> */}
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur impedit ab quisquam expedita? Tenetur nam cupiditate fugit incidunt. Harum officiis sapiente voluptatibus at minima minus est praesentium ipsum animi?
                     </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur impedit ab quisquam expedita? Tenetur nam cupiditate fugit incidunt. Harum officiis sapiente voluptatibus at minima minus est praesentium ipsum animi?
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,10 +1,28 @@
 import './Sidebar.css';
 import links from '../data/articles.json';
+import React from 'react';
 
 function Sidebar() {
+
     let a = 33;
-    function f1() {
-        console.log('f1 '+ a);
+
+    let textInput = React.createRef();
+    let textOutput = React.createRef();
+
+    function f1(number) {
+        console.log('f1 '+ number);
+    }
+
+    function f2(number) {
+        console.log('mousemove '+ number);
+    }
+
+    function showInput(event) {
+        console.log('input');
+        console.log(event.target.value);
+        console.log(textInput.current.value);
+        // console.log(this.value);
+        textOutput.current.innerHTML = textInput.current.value;
     }
     return (
         <div className="sidebar">
@@ -12,10 +30,15 @@ function Sidebar() {
             <section>
                 <h1>HAPPENING</h1>
                 <h2>Button</h2>
-                <button onClick={f1}>Push</button>
+                <button onClick={()=>f1(1)}>Push</button>
                 <h2>Double click + mousemove</h2>
-                <div className="cyanbox"></div>
+                <div className="cyanbox" onDoubleClick={f1} onMouseMove={() => f2(2)}></div>
+                <h3>Input</h3>
+                <input type="text" onInput={showInput} ref={textInput} defaultValue='lol' />
+                
+                <p ref={textOutput}></p>
             </section>
+
             <nav>
                 {/* <ul>
                     {links.map(li2 =>
