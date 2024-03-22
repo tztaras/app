357ba0d5f70ddb694c91704cab872c56ccd4d4ed
tarasend <tz.taras@gmail.com>
2024-03-22 17:47:51 +0200

    refs

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,12 +4,15 @@ import React from 'react';
 import { useState } from 'react';
 
 function Sidebar() {
-
+    
     let a = 33;
     let cd = 'cadetblue';
     let textInput = React.createRef();
     let textOutput = React.createRef();
+    let task4 = React.createRef();
+    let task4Out = React.createRef();
     const [output, setOutput] = useState('hello');
+    
     function f1(number) {
         console.log('f1 '+ number);
     }
@@ -19,13 +22,26 @@ function Sidebar() {
     }
 
     function showInput(event) {
-        console.log('input');
-        console.log(event.target.value);
-        console.log(textInput.current.value);
+        // console.log('input');
+        // console.log(event.target.value);
+        // console.log(textInput.current.value);
         // console.log(this.value);
         textOutput.current.innerHTML = textInput.current.value;
         setOutput(textInput.current.value);
+        setOutput(console.log(textInput.current.value));
+        // let sd = textInput.current.value;
+        // console.log(sd);
     }
+
+    function t4() {
+        let sd = task4.current.value;
+        task4Out.current.innerHTML = sd;
+        console.log(sd);
+    }
+
+    // function task3{
+    //     text
+    // }
     return (
         <div className="sidebar">
             
@@ -37,9 +53,11 @@ function Sidebar() {
                 <div className="cyanbox" onDoubleClick={f1} onMouseEnter={() => f2(2) } style={{background: cd}}></div>
                 <h3>Input</h3>
                 <input type="text" onInput={showInput} ref={textInput} defaultValue='lol' />
+                <input type="text" onInput={t4} ref={task4} defaultValue='lol2' />
                 
                 <p ref={textOutput}></p>
-                <p style={{ fontStyle: 'italic', background:'cadetblue' }}>{output}</p>
+                <p style={{ fontStyle: 'italic', background: 'cadetblue' }}>{output}</p>
+                <p ref={task4Out}></p>
             </section>
 
             <nav>
